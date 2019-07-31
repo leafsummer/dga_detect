973a263fc047b85042b2d45fa78af3cdf5f85aa8
leafsummer <wing.sun1987@gmail.com>
2019-07-31 16:42:21 +0800

    add the dga detect source code files

---
+++ dga_detect_lstm/dga_classifier/bigram.py
@@ -0,0 +1,92 @@
+"""Train and test bigram classifier"""
+import os
+import dga_classifier.data as data
+from keras.layers.core import Dense
+from keras.models import Sequential, save_model
+import sklearn
+from sklearn import feature_extraction
+from sklearn.model_selection import train_test_split
+
+save_path = os.path.join(data.proj_path, 'dga_bigram_classifier.h5')
+
+def build_model(max_features):
+    """Builds logistic regression model"""
+    model = Sequential()
+    model.add(Dense(1, input_dim=max_features, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam')
+
+    return model
+
+
+def run(max_epoch=50, nfolds=10, batch_size=128):
+    """Run train/test on logistic regression model"""
+    indata = data.get_data()
+
+    # Extract data and labels
+    X = [x[1] for x in indata]
+    labels = [x[0] for x in indata]
+
+    # Create feature vectors
+    print "vectorizing data"
+    ngram_vectorizer = feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(2, 2))
+    count_vec = ngram_vectorizer.fit_transform(X)
+
+    max_features = count_vec.shape[1]
+
+    # Convert labels to 0-1
+    y = [0 if x == 'benign' else 1 for x in labels]
+
+    final_data = []
+    nfolds_best_model = {'best_auc': 0.0,  'best_model': None}
+    for fold in range(nfolds):
+        print "fold %u/%u" % (fold+1, nfolds)
+        X_train, X_test, y_train, y_test, _, label_test = train_test_split(count_vec, y,
+                                                                           labels, test_size=0.2)
+
+        print 'Build model...'
+        model = build_model(max_features)
+
+        print "Train..."
+        X_train, X_holdout, y_train, y_holdout = train_test_split(X_train, y_train, test_size=0.05)
+        best_iter = -1
+        best_auc = 0.0
+        out_data = {}
+
+        for ep in range(max_epoch):
+            model.fit(X_train.todense(), y_train, batch_size=batch_size, epochs=1)
+
+            t_probs = model.predict_proba(X_holdout.todense())
+            t_auc = sklearn.metrics.roc_auc_score(y_holdout, t_probs)
+
+            print '\nEpoch %d: auc = %f (best=%f)' % (ep, t_auc, best_auc)
+
+            if t_auc > best_auc:
+                best_auc = t_auc
+                best_iter = ep
+
+                probs = model.predict_proba(X_test.todense())
+
+                out_data = {'y':y_test, 'labels': label_test, 'probs':probs, 'epochs': ep,
+                            'confusion_matrix': sklearn.metrics.confusion_matrix(y_test, probs > .5)}
+
+                print '\n', sklearn.metrics.confusion_matrix(y_test, probs > .5)
+            else:
+                # No longer improving...break and calc statistics
+                if (ep-best_iter) > 5:
+                    break
+
+        final_data.append(out_data)
+        nfolds_best_auc = nfolds_best_model['best_auc']
+        if best_auc > nfolds_best_auc:
+            nfolds_best_model['best_auc'] = best_auc
+            nfolds_best_model['best_model'] = model
+    best_model = nfolds_best_model['best_model']
+    save_model(best_model, save_path)
+    # print 'final_data: ', final_data 
+    return final_data
+
+
+if __name__ == '__main__':
+    final_data = run()
